4423d02bec8091a7db5bbcc47ff56e2b6933657e
alessandro crocetta <crocetta.2120774@studenti.uniroma1.edu.it>
2025-12-08 18:04:32 +0100

    added the login, setusername, getConversations endpoint

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -28,17 +28,18 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api"
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
 	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/globaltime"
 	"github.com/ardanlabs/conf"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/sirupsen/logrus"
-	"math/rand"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 )
 
 // main is the program entry point. The only purpose of this function is to call run() and set the exit code if there is
@@ -173,10 +174,8 @@ func run() error {
 
 		// Log the status of this shutdown.
 		switch {
-		case sig == syscall.SIGSTOP:
-			return errors.New("integrity issue caused shutdown")
 		case err != nil:
-			return fmt.Errorf("could not stop server gracefully: %w", err)
+			return fmt.Errorf("could not stop server gracefully: %w", err)
 		}
 	}
 
